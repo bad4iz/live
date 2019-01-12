f2297298347ca5e4b3e1c39baeee2666bdb99773
bad4iz <bad4iz@gmail.com>
2019-01-12 12:27:38 +0400

    g

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ class Vector {
     }
 }
 
+var directions = {
+    'n': new Vector( 0, -1),
+    'ne': new Vector( 1, -1),
+    'e': new Vector( 1,  0),
+    'se': new Vector( 1,  1),
+    's': new Vector( 0,  1),
+    'sw': new Vector(-1,  1),
+    'w': new Vector(-1,  0),
+    'nw': new Vector(-1, -1)
+};
+
 class Grid {
     constructor(width, height) {
         this.space = new Array(width * height);
@@ -36,17 +47,6 @@ class Grid {
         }
     }
 }
-
-var directions = {
-    'n': new Vector( 0, -1),
-    'ne': new Vector( 1, -1),
-    'e': new Vector( 1,  0),
-    'se': new Vector( 1,  1),
-    's': new Vector( 0,  1),
-    'sw': new Vector(-1,  1),
-    'w': new Vector(-1,  0),
-    'nw': new Vector(-1, -1)
-};
 
 function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
