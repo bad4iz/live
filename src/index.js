b5bedd152ea6ae5d4928bca4551404480e9058b8
bad4iz <bad4iz@gmail.com>
2019-03-09 21:08:21 +0400

    add editor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import Animated from './lib/animateworld';
+const ace = window.ace;
 
 import world from './script.js';
 
 new Animated(world);
+
+var editor = ace.edit('editor');
+editor.setTheme('ace/theme/dracula');
+editor.session.setMode('ace/mode/javascript');
