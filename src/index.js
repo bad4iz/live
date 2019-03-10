0e77f87f19d262c39f5ddaafcefb55881cad8fb5
bad4iz <bad4iz@gmail.com>
2019-03-10 20:13:01 +0400

    redactor and saving

--- src/index.js
+++ src/index.js
@@ -1,11 +1,69 @@
 import './style.css';
-import Animated from './lib/animateworld';
+import AnimatedTxt from './lib/animateworldtxt';
+// import Animated from './lib/animateworld';
+import {Vector, BouncingCritter, directions, functions, Grid, plan, View, World} from './scripttxt';
+
 const ace = window.ace;
 
-import world from './script.js';
+// import World, { Wall, BouncingCritter} from './script';
+// import world from './script';
+// import plan from './plan';
 
-new Animated(world);
 
 var editor = ace.edit('editor');
 editor.setTheme('ace/theme/dracula');
 editor.session.setMode('ace/mode/javascript');
+// var htmlEditor = ace.edit('html-editor');
+// htmlEditor.setTheme('ace/theme/monokai');
+// htmlEditor.session.setMode('ace/mode/html');
+
+
+const save = document.getElementById('save');
+// console.log(require('./scripttxt').default.toString());
+
+save.addEventListener('click', update);
+document.addEventListener('keydown', saving);
+
+function saving(event) {
+    if (event.ctrlKey && event.keyCode === 83) {
+        event.preventDefault();
+        update();
+    }
+}
+function update() {
+    // const text = Animated.toString() +
+    //     World.toString() +
+    //     Wall.toString() +
+    //     BouncingCritter.toString() +
+    //     plan.toString() +
+    // `
+    // var world = new World(plan, {
+    //     '#': Wall,
+    //     o: BouncingCritter,
+    //     // '=': WallFollower
+    // });\
+    const iframe = document.getElementById('result');
+    if (iframe) {
+        iframe.remove();
+    }
+    const result = document.createElement('iframe');
+    result.id = 'result';
+    document.body.appendChild(result);
+    // var res = document.getElementById('result').contentWindow.document;
+    var res = result.contentWindow.document;
+    res.open();
+    res.write('<div>jopa</div>');
+    res.open();
+    res.write('<div>jopa</div>');
+    res.write('<script>' + AnimatedTxt + Vector + directions + Grid + functions + World + View + BouncingCritter + '</script>');
+
+    res.write('<script>' + editor.getValue()  + '</script>');
+    res.write('<script>' +  plan + '</script>');
+    res.write('<script>var world = new World(plan, {\n    \'#\': Wall,\n    o: BouncingCritter,\n    \'=\': WallFollower\n});</script>');
+
+    res.write('<script>new Animated(world)</script>');
+
+
+
+    res.close();
+}
